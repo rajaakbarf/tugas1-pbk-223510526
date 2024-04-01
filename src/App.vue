0ddbfd8ca59dad1a8fcd2ab86da64363500ac08c
rajaakbarf <akbarfebriano86@gmail.com>
2024-04-01 23:00:20 +0700

    tugas 1

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    <img src="./assets/uirfoto.jpg"  width="125" height="125" style="border-radius: 50%;object-fit: cover;" />
+
+    <div class="wrapper">
+      <HelloWorld msg="Raja Akbar Febriano" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,68 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Education</template>
+
+    Informatics Engineering UIR
+    <a href="https://vuejs.org/" target="_blank" rel="noopener">(2022-2026)</a>
+    
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Project</template>
+
+    Making UI/UX Design from the Application called
+    <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener">HairTime</a>.
+    Which is the RPL's project
+    
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Expertise</template>
+
+    
+    <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Figma</a>,
+    <a href="https://router.vuejs.org/" target="_blank" rel="noopener">Canva</a>.
+  
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>Languages</template>
+
+   INDONESIA
+    <a href="https://chat.vuejs.org" target="_blank" rel="noopener">100%</a>, ENGLISH
+    <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"
+      >65%</a
+    >.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>CONTACT</template>
+
+    EMAIL : 
+    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">akbarfebriano@gmail.com</a>.
+  </WelcomeItem>
+</template>
